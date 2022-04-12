fbcb30cb70fdfa858c6a3a421bc31eb752a2ee26
mnz63 <danilocsi88@gmail.com>
2022-04-12 19:22:00 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,31 @@
+var tema = false;
+let a = document.getElementById('a')
+let add =  document.getElementById('add')
+let cmd =  document.getElementById('cmd')
+
+function clicarBotao() {
+   if (!tema) {
+        a.style.background = 'url(https://raw.githubusercontent.com/danilomnZh63/siteakita/main/1.png)' // imagem dark
+
+    tema = true;
+  } else {
+    a.style.background = 'url(https://raw.githubusercontent.com/danilomnZh63/siteakita/main/2.png)' // imagem padrao
+    tema = false;
+    }
+}
+
+function github() {
+  window.location.href = 'https://github.com/mnz63'
+}
+
+function adicionar() {
+  window.location.href  = 'https://discord.com/oauth2/authorize?client_id=954560283580465192&permissions=8&scope=bot'
+}
+
+function mudarcor() {
+  cmd.style.background = '#a1a1a1'
+}
+
+function mudarcor2() {
+  cmd.style.background = '#ffffff'
+}
